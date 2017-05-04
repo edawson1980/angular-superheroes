6ecd6a9652ba07f9adef914826ad5eb00f5df570
Erin <erinedawson@gmail.com>
2017-05-04 16:21:37 -0400

    add class to colorcode by affiliation

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var superheroList = [
   {name: "Black Panther", number: "800-TCH-ALLA", group: "Marvel", img: "https://cdn.bleedingcool.net/wp-content/uploads/2016/04/0110-600x566.jpg"},
   {name: "Catwoman", number: "800-NO-DAMSL", group: "DC", img: "https://s-media-cache-ak0.pinimg.com/originals/90/0e/1f/900e1f5f35fd9641646c65222611fe8c.jpg"},
   {name: "Batman", number: "800-DRK-NITE", group: "DC", img: "https://comicsauthorityblog.files.wordpress.com/2013/10/arkham_asylum_entrance.jpg"},
-  {name: "TaNk GiRl",number: "800-WTR-NPWR",group: "No Masters",img: "https://vignette4.wikia.nocookie.net/tankgirl/images/0/0a/Hewl-Tanky.jpg/revision/latest?cb=20121002235926"}
+  {name: "TaNk GiRl",number: "800-WTR-NPWR",group: "None",img: "https://vignette4.wikia.nocookie.net/tankgirl/images/0/0a/Hewl-Tanky.jpg/revision/latest?cb=20121002235926"}
 ];
 
 
